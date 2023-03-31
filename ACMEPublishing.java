7a23af373c40be431ef82407be6f1a2ea3ece113
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-03-31 17:49:27 -0300

    upd: added commentaries and variable name changes

--- ACMEPublishing.java
+++ ACMEPublishing.java
@@ -16,10 +16,10 @@ public class ACMEPublishing {
 	// Construtor
 	public ACMEPublishing() {
 		try {
-			BufferedReader streamEntrada = new BufferedReader(new FileReader("src/dados.txt"));
+			BufferedReader streamEntrada = new BufferedReader(new FileReader("dados.txt"));
 			entrada = new Scanner(streamEntrada); // Usa como entrada um arquivo
 			PrintStream streamSaida =
-					new PrintStream(new File("src/saida.txt"), Charset.forName("UTF-8"));
+					new PrintStream(new File("saida.txt"), Charset.forName("UTF-8"));
 			System.setOut(streamSaida); // Usa como saida um arquivo
 		} catch (Exception e) {
 			System.out.println(e);
@@ -28,44 +28,46 @@ public class ACMEPublishing {
 	}
 
 	public void executa() {
+		// PASSO 1: CADASTRAR LIVROS
 		while (entrada.hasNextLine()) {
-			String id = entrada.nextLine();
-			if (id.equals("-1")) {
+			String id = entrada.nextLine(); // isbn lido do livro
+			if (id.equals("-1")) { // sempre que ler "-1", ir para prox funcionalidade
 				break;
 			}
+			// proximas 2 linhas depois do Id vao ser Nome e Ano respectivamente
 			String infoLivro = entrada.nextLine() + "," + entrada.nextLine();
 			String[] campos = infoLivro.split(",");
 			String nome = campos[0];
 			String ano = campos[1];
 
-			// Do something with the book information
+			// testes no arquivo de saido
 			System.out.println("ISBN LIVRO: " + id);
 			System.out.println("NOME LIVRO: " + nome);
 			System.out.println("ANO DE LANCAMENTO: " + ano);
 		}
 
-
+		// PASSO 3: CADASTRAR AUTORES
 		while (entrada.hasNextLine()) {
-			String id = entrada.nextLine();
+			String id = entrada.nextLine(); // Id lido do Autor
 			if (id.equals("-1")) {
 				break;
 			}
+			// proximas 2 linhas depois do Id vao ser Nome e isbn respectivamente
 			String infoAutores = entrada.nextLine() + "," + entrada.nextLine();
 			String[] campos = infoAutores.split(",");
 			String nome = campos[0];
-			String ano = campos[1];
+			String isbn = campos[1];
 
 			// Do something with the book information
-			System.out.println("ISBN LIVRO: " + id);
-			System.out.println("NOME LIVRO: " + nome);
-			System.out.println("ANO DE LANCAMENTO: " + ano);
+			System.out.println("ID AUTOR: " + id);
+			System.out.println("NOME AUTOR: " + nome);
+			System.out.println("ISBN LIVRO: " + isbn);
 		}
 		while (entrada.hasNextLine()) { // TODO: UPDATE ME WITH ABOVE LOGIC
 			String line = entrada.nextLine();
 			if (line.equals("-1"))
 				break;
-			System.out.println("ID AUTOR: " + line); // essa vai ser o ID
-			System.out.println("ISBN LIVRO: " + entrada.nextLine()); // proxima linha depois do Id, nome
+
 		}
 	}
 
