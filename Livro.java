839d2fe09dcfdd5234681c0a5ca81fa80ff6a186
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-03-31 16:52:22 -0300

    upd: added getters for class variables

--- Livro.java
+++ Livro.java
@@ -10,6 +10,22 @@ public class Livro {
 
 	private ArrayList<Autor> autores;
 
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public ArrayList<Autor> getAutores() {
+		return autores;
+	}
+
 	public boolean adicionaAutor(Autor autor) {
 		autores.add(autor);
 		return false;
