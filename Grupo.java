14502be8b669c8d8019d5fe43d6d098324613dbf
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-03-31 16:50:14 -0300

    upd: implemented methods and changed class variable type

--- Grupo.java
+++ Grupo.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
+
 public class Grupo {
 
-	private Autor[] autor;
+	private ArrayList<Autor> autores;
 
 	public boolean cadastraAutor(Autor autor) {
+		int auxPesquisa = autor.getCodigo();
+		if (pesquisaAutor(auxPesquisa) != null) {
+			autores.add(autor);
+			return true;
+		}
 		return false;
 	}
 
 	public Autor pesquisaAutor(int codigo) {
+		for (Autor autor : autores) {
+			if (autor.getCodigo() == codigo) {
+				return autor;
+			}
+		}
 		return null;
 	}
 
